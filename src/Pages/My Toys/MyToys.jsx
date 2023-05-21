b891e70620c98a85efa2ca083c9c6b6c8599fb93
faizulsami <faizul.sami2021@gmail.com>
2023-05-22 03:13:18 +0600

    Sweet Aleart Install and Added on Delete Button

--- src/Pages/My Toys/MyToys.jsx
+++ src/Pages/My Toys/MyToys.jsx
@@ -1,12 +1,13 @@
 import { useContext, useEffect, useState } from "react";
-import { useLoaderData } from "react-router-dom";
+import { Link, useLoaderData } from "react-router-dom";
 import { AuthContext } from "../../Provider/AuthProvider";
 import { Helmet } from "react-helmet-async";
+import Swal from "sweetalert2";
 
 const MyToys = () => {
-    const {user} = useContext(AuthContext);
+    const { user } = useContext(AuthContext);
     const [myToys, setToys] = useState([]);
-    useEffect(()=>{
+    useEffect(() => {
         fetch(`https://assignment-11-server-eight-eosin.vercel.app/myToys?sellerEmail=${user?.email}`, {
             method: 'GET',
             headers: {
@@ -17,7 +18,34 @@ const MyToys = () => {
             .then(data => {
                 setToys(data);
             })
-    },[user]);
+    }, [user]);
+    const handleDelete = id => {
+        Swal.fire({
+            title: 'Are you sure?',
+            text: "You won't be able to revert this!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Yes, delete it!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`https://assignment-11-server-eight-eosin.vercel.app/allToys/${id}`,{
+                    method:'DELETE'
+                })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deleteCount > 0) {
+                        Swal.fire(
+                            'Deleted!',
+                            'Your Toy has been deleted.',
+                            'success'
+                        )
+                    }
+                })
+            }
+        })
+    }
     return (
         <div className="container mx-auto">
             <Helmet>
@@ -37,17 +65,17 @@ const MyToys = () => {
                                 Detail description
                             </th>
                             <th scope="col" className="px-6 py-3">
-                                
+
                             </th>
                             <th scope="col" className="px-6 py-3">
-                                
+
                             </th>
                         </tr>
                     </thead>
                     <tbody>
                         {
                             myToys.map(toys => {
-                                const {price, quantity, _id,description } = toys;
+                                const { price, quantity, _id, description } = toys;
                                 return (
                                     <tr key={_id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
                                         <th scope="row" className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white">
@@ -63,7 +91,9 @@ const MyToys = () => {
 
                                         </td>
                                         <td className="px-6 py-4">
-                                            
+                                            <button
+                                                onClick={() => handleDelete(_id)}
+                                                type="button" className="text-white color5 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 ">Delete</button>
                                         </td>
                                     </tr>
                                 )
---
+++ src/Pages/Update Toys/UpdateToys.jsx
@@ -0,0 +1,9 @@
+const UpdateToys = () => {
+    return (
+        <div>
+            <h2></h2>
+        </div>
+    );
+};
+
+export default UpdateToys;
